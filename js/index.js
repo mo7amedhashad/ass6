580f63880cd621fd23d1146340c8675b848db606
mo7amedhashad <mohamedhashad178@gmail.com>
2026-04-27 01:51:42 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,69 @@
+// var day = 1;
+// if (day == 1) {
+//   console.log("day is sunday");
+// } else if (day == 2) {
+//   console.log("day is monday");
+// } else if (day == 3) {
+//   console.log("day is thursday");
+// } else if (day == 4) {
+//   console.log("day is wednesday");
+// } else if (day == 5) {
+//   console.log("day is thursday");
+// } else {
+//   console.log("off day");
+// }
+
+// var x = 1;
+// if (x > 0) {
+//   console.log("x is positive number");
+// } else if (x < 0) {
+//   console.log("x is negative number");
+// } else if (x == 0) {
+//   console.log("x is equal to 0");
+// }
+var day = 5;
+switch (day) {
+  case 1:
+    console.log("day is sunday");
+    break;
+  case 2:
+    console.log("day is monday");
+    break;
+  case 3:
+    console.log("day is tusday");
+    break;
+  case 4:
+    console.log("day is wendesday");
+    break;
+  case 5:
+    console.log("day is thrsday");
+    break;
+    default:
+        console.log("off day")
+}
+
+var letter='a';
+switch(letter){
+case 'a':
+    console .log("vowel")
+    break;
+case 'i':
+    console .log("vowel")
+    break;
+case 'u':
+    console .log("vowel")
+    break;
+case 'e':
+    console .log("vowel")
+    break;
+case 'o':
+    console .log("vowel")
+    break;
+default:
+    co
+
+
+
+
+
+}
